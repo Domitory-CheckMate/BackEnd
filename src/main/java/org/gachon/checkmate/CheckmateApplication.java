f48e643d446fdc5593dbf1af36154ab3cd1146c9
RyuKwanKon <rhks33rhs@gmail.com>
2023-12-23 16:56:06 +0900

    [chore] #1 프로젝트 생성

---
+++ src/main/java/org/gachon/checkmate/CheckmateApplication.java
@@ -0,0 +1,13 @@
+package org.gachon.checkmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gachon/checkmate/CheckmateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gachon.checkmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
