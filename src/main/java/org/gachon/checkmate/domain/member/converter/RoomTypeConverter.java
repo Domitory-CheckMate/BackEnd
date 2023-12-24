7221984761f3aed994ebe1d6006e37e204c6b292
RyuKwanKon <rhks33rhs@gmail.com>
2023-12-24 22:59:16 +0900

    [feat] #1 기숙사 type enum class 구현

---
+++ src/main/java/org/gachon/checkmate/domain/member/converter/RoomTypeConverter.java
@@ -0,0 +1,12 @@
+package org.gachon.checkmate.domain.member.converter;
+
+import jakarta.persistence.Converter;
+import org.gachon.checkmate.domain.member.entity.RoomType;
+import org.gachon.checkmate.global.utils.AbstractEnumCodeAttributeConverter;
+
+@Converter
+public class RoomTypeConverter extends AbstractEnumCodeAttributeConverter<RoomType> {
+    public RoomTypeConverter() {
+        super(RoomType.class);
+    }
+}
---
+++ src/main/java/org/gachon/checkmate/domain/member/entity/RoomType.java
@@ -0,0 +1,15 @@
+package org.gachon.checkmate.domain.member.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.gachon.checkmate.global.utils.EnumField;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum RoomType implements EnumField {
+    TWO("1", "2인실"),
+    FOUR("2", "4인실");
+    private String code;
+    private String desc;
+}
