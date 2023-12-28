ed8ab21dd7f3caf7f708d63fa9b79be2ffb39909
RyuKwanKon <rhks33rhs@gmail.com>
2023-12-28 23:10:06 +0900

    [feat] #8 querydsl config 구현

---
+++ src/main/java/org/gachon/checkmate/global/config/QuerydslConfig.java
@@ -0,0 +1,17 @@
+package org.gachon.checkmate.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @Autowired
+    EntityManager em;
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
