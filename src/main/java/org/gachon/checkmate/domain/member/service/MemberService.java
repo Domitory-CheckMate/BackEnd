03999b7c355b90a8ba691cb0a5f90b20a3abca73
ziiyouth <ziy913@naver.com>
2023-12-30 18:02:19 +0900

    [feat] #10 회원가입 API service

--- src/main/java/org/gachon/checkmate/domain/member/service/MemberService.java
+++ src/main/java/org/gachon/checkmate/domain/member/service/MemberService.java
@@ -3,9 +3,14 @@ package org.gachon.checkmate.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.gachon.checkmate.domain.member.dto.request.EmailPostRequestDto;
+import org.gachon.checkmate.domain.member.dto.request.MemberSignUpRequestDto;
 import org.gachon.checkmate.domain.member.dto.response.EmailResponseDto;
+import org.gachon.checkmate.domain.member.dto.response.MemberSignUpResponseDto;
+import org.gachon.checkmate.domain.member.entity.User;
+import org.gachon.checkmate.domain.member.repository.UserRepository;
 import org.gachon.checkmate.global.config.auth.jwt.JwtProvider;
 import org.gachon.checkmate.global.config.mail.MailProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +22,44 @@ public class MemberService {
 
     private final JwtProvider jwtProvider;
     private final MailProvider mailProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public EmailResponseDto sendMail(EmailPostRequestDto emailPostRequestDto) {
         String authNum = mailProvider.sendMail(emailPostRequestDto.email(), "email");
         return new EmailResponseDto(authNum);
+    }
+
+    public MemberSignUpResponseDto signUp(MemberSignUpRequestDto memberSignUpRequestDto) {
+        Long newMemberId = createMember(memberSignUpRequestDto);
+        String accessToken = issueNewToken(newMemberId);
+        String refreshToken = issueNewToken(newMemberId);
+        return MemberSignUpResponseDto.of(memberSignUpRequestDto.name(), accessToken, refreshToken);
+    }
+
+    private String issueNewToken(Long memberId) {
+        return jwtProvider.getIssueToken(memberId, true);
+    }
+
+    private Long createMember(MemberSignUpRequestDto memberSignUpRequestDto) {
+        User newUser = User.createUser(
+                memberSignUpRequestDto.email(),
+                encodedPassword(memberSignUpRequestDto.password()),
+                memberSignUpRequestDto.name(),
+                memberSignUpRequestDto.school(),
+                memberSignUpRequestDto.major(),
+                memberSignUpRequestDto.mbtiType(),
+                memberSignUpRequestDto.genderType()
+        );
+        return userRepository.save(newUser).getId();
+    }
+
+    private String encodedPassword(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    private boolean authenticatePassword(String enteredPassword, String storedPassword) {
+        return passwordEncoder.matches(enteredPassword, storedPassword);
     }
 
 }
