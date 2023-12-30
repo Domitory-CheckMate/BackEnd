37445446fbb2d746e45dbeaac31a8a19f27af59e
ziiyouth <ziy913@naver.com>
2023-12-30 19:16:17 +0900

    [feat] #10 로그인 기능 service 구현

--- src/main/java/org/gachon/checkmate/domain/member/repository/UserRepository.java
+++ src/main/java/org/gachon/checkmate/domain/member/repository/UserRepository.java
@@ -3,6 +3,9 @@ package org.gachon.checkmate.domain.member.repository;
 import org.gachon.checkmate.domain.member.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/org/gachon/checkmate/domain/member/service/MemberService.java
+++ src/main/java/org/gachon/checkmate/domain/member/service/MemberService.java
@@ -3,16 +3,23 @@ package org.gachon.checkmate.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.gachon.checkmate.domain.member.dto.request.EmailPostRequestDto;
+import org.gachon.checkmate.domain.member.dto.request.MemberSignInRequestDto;
 import org.gachon.checkmate.domain.member.dto.request.MemberSignUpRequestDto;
 import org.gachon.checkmate.domain.member.dto.response.EmailResponseDto;
+import org.gachon.checkmate.domain.member.dto.response.MemberSignInResponseDto;
 import org.gachon.checkmate.domain.member.dto.response.MemberSignUpResponseDto;
 import org.gachon.checkmate.domain.member.entity.User;
 import org.gachon.checkmate.domain.member.repository.UserRepository;
 import org.gachon.checkmate.global.config.auth.jwt.JwtProvider;
 import org.gachon.checkmate.global.config.mail.MailProvider;
+import org.gachon.checkmate.global.error.exception.EntityNotFoundException;
+import org.gachon.checkmate.global.error.exception.UnauthorizedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.gachon.checkmate.global.error.ErrorCode.INVALID_PASSWORD;
+import static org.gachon.checkmate.global.error.ErrorCode.USER_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,6 +42,16 @@ public class MemberService {
         String accessToken = issueNewToken(newMemberId);
         String refreshToken = issueNewToken(newMemberId);
         return MemberSignUpResponseDto.of(newMemberId, memberSignUpRequestDto.name(), accessToken, refreshToken);
+    }
+
+    public MemberSignInResponseDto signIn(MemberSignInRequestDto memberSignInRequestDto){
+        User user = getUserFromEmail(memberSignInRequestDto.email());
+        if (!authenticatePassword(memberSignInRequestDto.password(), user.getPassword())) {
+            throw new UnauthorizedException(INVALID_PASSWORD);
+        }
+        String accessToken = issueNewToken(user.getId());
+        String refreshToken = issueNewToken(user.getId());
+        return MemberSignInResponseDto.of(user.getId(), accessToken, refreshToken);
     }
 
     private String issueNewToken(Long memberId) {
@@ -61,5 +78,11 @@ public class MemberService {
     private boolean authenticatePassword(String enteredPassword, String storedPassword) {
         return passwordEncoder.matches(enteredPassword, storedPassword);
     }
+
+    private User getUserFromEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND));
+    }
+
 
 }
