d52695b473e48577982052a5be48eca3221e65ab
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-03 20:16:15 +0900

    [feat] #15 custom paging util 구현

---
+++ src/main/java/org/gachon/checkmate/global/utils/PagingUtils.java
@@ -0,0 +1,20 @@
+package org.gachon.checkmate.global.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.gachon.checkmate.global.error.exception.InvalidValueException;
+
+import java.util.List;
+
+import static org.gachon.checkmate.global.error.ErrorCode.INVALID_PAGING_SIZE;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PagingUtils {
+    public static <T> List<T> convertPaging(List<T> dataList, long page, int size) {
+        if (dataList.size() <= page * size)
+            throw new InvalidValueException(INVALID_PAGING_SIZE);
+        int startIndex = (int) page * size;
+        int endIndex = Math.min(dataList.size(), (int) page * (size + 1));
+        return dataList.subList(startIndex, endIndex);
+    }
+}
