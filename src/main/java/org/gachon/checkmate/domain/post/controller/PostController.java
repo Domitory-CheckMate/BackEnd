39ce2f43745b66c3d4e67238cf8d3d29a1ab73b5
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-04 21:33:45 +0900

    [feat] #20 게시글 상세 조회 api 구현

--- src/main/java/org/gachon/checkmate/domain/post/controller/PostController.java
+++ src/main/java/org/gachon/checkmate/domain/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package org.gachon.checkmate.domain.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.gachon.checkmate.domain.post.dto.response.PostDetailResponseDto;
 import org.gachon.checkmate.domain.post.dto.response.PostSearchResponseDto;
 import org.gachon.checkmate.domain.post.service.PostService;
 import org.gachon.checkmate.global.common.SuccessResponse;
@@ -18,17 +19,14 @@ public class PostController {
     @GetMapping
     public ResponseEntity<SuccessResponse<?>> getAllPosts(@UserId final Long userId,
                                                           @RequestParam final String type,
-                                                          final Pageable pageable){
+                                                          final Pageable pageable) {
         final PostSearchResponseDto responseDto = postService.getAllPosts(userId, type, pageable);
         return SuccessResponse.ok(responseDto);
     }
 
-    @GetMapping("/{key}")
-    public ResponseEntity<SuccessResponse<?>> searchKeyWordPost(@UserId final Long userId,
-                                                                @PathVariable final String key,
-                                                                @RequestParam final String type,
-                                                                final Pageable pageable) {
-        final PostSearchResponseDto responseDto = postService.searchKeyWordPost(userId, key, type, pageable);
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<?>> getPostDetails(@PathVariable("id") final Long postId) {
+        final PostDetailResponseDto responseDto = postService.getPostDetails(postId);
         return SuccessResponse.ok(responseDto);
     }
 
@@ -37,6 +35,15 @@ public class PostController {
                                                              @RequestParam final String text,
                                                              final Pageable pageable) {
         final PostSearchResponseDto responseDto = postService.searchTextPost(userId, text, pageable);
+        return SuccessResponse.ok(responseDto);
+    }
+
+    @GetMapping("/search/{key}")
+    public ResponseEntity<SuccessResponse<?>> searchKeyWordPost(@UserId final Long userId,
+                                                                @PathVariable final String key,
+                                                                @RequestParam final String type,
+                                                                final Pageable pageable) {
+        final PostSearchResponseDto responseDto = postService.searchKeyWordPost(userId, key, type, pageable);
         return SuccessResponse.ok(responseDto);
     }
 }
