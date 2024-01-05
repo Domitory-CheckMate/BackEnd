0d0882895d89d1f31abc2d912da4ff56b62f99b2
OJOJIN <ojy101039@gmail.com>
2024-01-05 19:15:23 +0900

    [feat] #5 mongodb config 추가

---
+++ src/main/java/org/gachon/checkmate/global/config/MongoDbConfig.java
@@ -0,0 +1,33 @@
+package org.gachon.checkmate.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "org.gachon.checkmate.domain.chat.mongorepository")
+public class MongoDbConfig {
+
+    private final MongoMappingContext mongoMappingContext;
+
+    // 데이터 저장시 "_class" 필드가 자동 추가되는 현상 방지를 위한 설정
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory mongoDatabaseFactory,
+                                                       MongoMappingContext mongoMappingContext) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+
+}
