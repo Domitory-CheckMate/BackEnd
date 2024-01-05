fa9cc8991b377a404bc08dc2a5de076c49e23328
OJOJIN <ojy101039@gmail.com>
2024-01-05 19:20:35 +0900

    [feat] #5 ChatRoom 관련 repository 생성

---
+++ src/main/java/org/gachon/checkmate/domain/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package org.gachon.checkmate.domain.chat.repository;
+
+import org.gachon.checkmate.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
+    List<ChatRoom> findAllByFirstMemberIdOrSecondMemberId(Long firstMemberId, Long secondMemberId);
+}
