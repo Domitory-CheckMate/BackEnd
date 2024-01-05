aaa7e2aa8d43cbca86a0038887cee7ba2e8303ad
OJOJIN <ojy101039@gmail.com>
2024-01-05 19:15:08 +0900

    [fix] #5 jpa config 범위 변경

--- src/main/java/org/gachon/checkmate/global/config/JpaConfig.java
+++ src/main/java/org/gachon/checkmate/global/config/JpaConfig.java
@@ -2,9 +2,11 @@ package org.gachon.checkmate.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing
 @Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "org.gachon.checkmate.domain.*.repository")
 public class JpaConfig {
 }
 
