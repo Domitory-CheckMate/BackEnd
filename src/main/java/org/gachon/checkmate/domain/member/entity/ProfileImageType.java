420f78f746f67eea88f7bcaa9b1ae8368ba8c29b
ziiyouth <ziy913@naver.com>
2024-01-05 16:59:37 +0900

    [feat] #22 프로필 사진 url 추가

--- src/main/java/org/gachon/checkmate/domain/member/entity/ProfileImageType.java
+++ src/main/java/org/gachon/checkmate/domain/member/entity/ProfileImageType.java
@@ -9,8 +9,8 @@ import lombok.Getter;
 public enum ProfileImageType {
 
     PROFILE_1("https://checkmate-dormitory-bucket.s3.ap-northeast-2.amazonaws.com/checkmate-profile1.png"),
-    PROFILE_2("https://example.com/profile2.jpg"),
-    PROFILE_3("https://example.com/profile3.jpg");
+    PROFILE_2("https://checkmate-dormitory-bucket.s3.ap-northeast-2.amazonaws.com/checkmate-profile2.png"),
+    PROFILE_3("https://checkmate-dormitory-bucket.s3.ap-northeast-2.amazonaws.com/checkmate-profile3.png");
 
     private final String imageUrl;
 }
