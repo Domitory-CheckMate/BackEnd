757890fa58fa22ea3a497eb2351f4d993a1a7e99
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-06 17:53:16 +0900

    [fix] #25 enum class not null check로 변경

--- src/main/java/org/gachon/checkmate/domain/post/dto/request/PostCreateRequestDto.java
+++ src/main/java/org/gachon/checkmate/domain/post/dto/request/PostCreateRequestDto.java
@@ -14,10 +14,10 @@ import java.time.LocalDate;
 public record PostCreateRequestDto(
         @NotBlank(message = "제목을 입력해주세요") String title,
         @NotBlank(message = "내용을 입력해주세요") String content,
-        @NotBlank(message = "중요 키워드를 입력해주세요") ImportantKeyType importantKey,
-        @NotBlank(message = "유사도를 입력해주세요") SimilarityKeyType similarityKey,
-        @NotBlank(message = "기숙사 유형을 입력해주세요") RoomType roomType,
-        @NotBlank(message = "모집 마감기간을 입력해주세요") LocalDate endDate,
+        @NotNull(message = "중요 키워드를 입력해주세요") ImportantKeyType importantKey,
+        @NotNull(message = "유사도를 입력해주세요") SimilarityKeyType similarityKey,
+        @NotNull(message = "기숙사 유형을 입력해주세요") RoomType roomType,
+        @NotNull(message = "모집 마감기간을 입력해주세요") LocalDate endDate,
         @NotNull(message = "체크리스트를 입력해주세요") CheckListRequestDto checkList
 ) {
 }
