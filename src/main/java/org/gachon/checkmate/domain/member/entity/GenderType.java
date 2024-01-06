22b7f93f4c131838e5a47616de80754295ffd692
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-06 21:12:57 +0900

    [refactor] #27 gender 한국어로 변경

--- src/main/java/org/gachon/checkmate/domain/member/entity/GenderType.java
+++ src/main/java/org/gachon/checkmate/domain/member/entity/GenderType.java
@@ -8,8 +8,8 @@ import org.gachon.checkmate.global.utils.EnumField;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public enum GenderType implements EnumField {
-    MAN("1", "man"),
-    WOMAN("2", "woman");
+    MAN("1", "남자"),
+    WOMAN("2", "여자");
 
     private final String code;
     private final String desc;
