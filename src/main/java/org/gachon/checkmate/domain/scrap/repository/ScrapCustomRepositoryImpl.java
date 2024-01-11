78a6fa3b0380c4fb1eef45ee5c0e9cce8e05cb9c
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-12 00:00:58 +0900

    [feat] #34 scrap query dsl 추상화 및 구현

---
+++ src/main/java/org/gachon/checkmate/domain/scrap/repository/ScrapCustomRepositoryImpl.java
@@ -0,0 +1,70 @@
+package org.gachon.checkmate.domain.scrap.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.gachon.checkmate.domain.member.entity.UserState;
+import org.gachon.checkmate.domain.post.dto.support.PostSearchDto;
+import org.gachon.checkmate.domain.post.dto.support.QPostSearchDto;
+import org.gachon.checkmate.domain.post.entity.Post;
+import org.gachon.checkmate.domain.scrap.dto.support.ScrapSearchCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.gachon.checkmate.domain.checkList.entity.QPostCheckList.postCheckList;
+import static org.gachon.checkmate.domain.member.entity.QUser.user;
+import static org.gachon.checkmate.domain.post.entity.QPost.post;
+import static org.gachon.checkmate.domain.scrap.entity.QScrap.scrap;
+
+@RequiredArgsConstructor
+public class ScrapCustomRepositoryImpl implements ScrapCustomRepository {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<PostSearchDto> searchMyScrapPosts(ScrapSearchCondition condition) {
+        List<PostSearchDto> content = queryFactory
+                .select(new QPostSearchDto(
+                        post.id,
+                        post.title,
+                        post.content,
+                        post.importantKeyType,
+                        post.similarityKeyType,
+                        post.endDate,
+                        post.scrapList.size(),
+                        postCheckList,
+                        user.gender
+                ))
+                .from(scrap)
+                .leftJoin(scrap.post, post)
+                .leftJoin(scrap.post.postCheckList, postCheckList)
+                .leftJoin(scrap.user, user)
+                .where(
+                        eqUserId(condition.userId()),
+                        validateUserState(),
+                        validatePostDate()
+                )
+                .offset(condition.pageable().getOffset())
+                .limit(condition.pageable().getPageSize())
+                .fetch();
+
+        JPAQuery<Post> countQuery = queryFactory
+                .selectFrom(post);
+        return PageableExecutionUtils.getPage(content, condition.pageable(), countQuery::fetchCount);
+    }
+
+    private BooleanExpression eqUserId(Long userId) {
+        return userId != null ? user.id.eq(userId) : null;
+    }
+
+    private BooleanExpression validateUserState() {
+        return user.userState.eq(UserState.JOIN);
+    }
+
+    private BooleanExpression validatePostDate() {
+        return post.endDate.after(LocalDate.now());
+    }
+}
