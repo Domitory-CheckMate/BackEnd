1731a3b65ebd644b23b1fe0abf0ada46be15a05f
ziiyouth <ziy913@naver.com>
2024-01-12 19:04:09 +0900

    [feat] #40 SmokeType, converter 추가

---
+++ src/main/java/org/gachon/checkmate/domain/checkList/entity/SmokeType.java
@@ -0,0 +1,17 @@
+package org.gachon.checkmate.domain.checkList.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.gachon.checkmate.global.utils.EnumField;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum SmokeType implements EnumField {
+    SMOKE("1", "흡연자 선호"),
+    NONE("2", "비흡연자 선호");
+
+    private final String code;
+    private final String desc;
+}
+
