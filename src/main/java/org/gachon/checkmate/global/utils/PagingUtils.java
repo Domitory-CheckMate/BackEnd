a68e2ade6953cd894d28dc7b6a0d50dd0d21f547
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-21 23:50:57 +0900

    [fix] #55 필터에 해당하는 게시글이 없을 경우 예외 로직 추가

--- src/main/java/org/gachon/checkmate/global/utils/PagingUtils.java
+++ src/main/java/org/gachon/checkmate/global/utils/PagingUtils.java
@@ -2,19 +2,31 @@ package org.gachon.checkmate.global.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.gachon.checkmate.global.error.exception.EntityNotFoundException;
 import org.gachon.checkmate.global.error.exception.InvalidValueException;
 
 import java.util.List;
 
 import static org.gachon.checkmate.global.error.ErrorCode.INVALID_PAGING_SIZE;
+import static org.gachon.checkmate.global.error.ErrorCode.POST_LIST_NOT_FOUND;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PagingUtils {
     public static <T> List<T> convertPaging(List<T> dataList, long offset, int size) {
-        if (dataList.size() <= offset)
-            throw new InvalidValueException(INVALID_PAGING_SIZE);
+        validFilterResult(dataList);
+        validOffsetSize(dataList, offset);
         int startIndex = (int) offset;
         int endIndex = Math.min(dataList.size(), startIndex + size);
         return dataList.subList(startIndex, endIndex);
+    }
+
+    private static <T> void validFilterResult(List<T> dataList) {
+        if (dataList.size() == 0)
+            throw new EntityNotFoundException(POST_LIST_NOT_FOUND);
+    }
+
+    private static <T> void validOffsetSize(List<T> dataList, long offset){
+        if (dataList.size() <= offset)
+            throw new InvalidValueException(INVALID_PAGING_SIZE);
     }
 }
