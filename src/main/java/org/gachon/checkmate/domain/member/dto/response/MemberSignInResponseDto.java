20fb53be0dc5b55316ee2a4f7df5697527620e45
RyuKwanKon <rhks33rhs@gmail.com>
2024-01-22 00:05:27 +0900

    [fix] #55 로그인시 성별 추가

--- src/main/java/org/gachon/checkmate/domain/member/dto/response/MemberSignInResponseDto.java
+++ src/main/java/org/gachon/checkmate/domain/member/dto/response/MemberSignInResponseDto.java
@@ -1,18 +1,22 @@
 package org.gachon.checkmate.domain.member.dto.response;
 
 import lombok.Builder;
+import org.gachon.checkmate.domain.member.entity.GenderType;
 
 @Builder
 public record MemberSignInResponseDto(
         Long memberId,
+        String gender,
         String accessToken,
         String refreshToken
 ) {
     public static MemberSignInResponseDto of(Long memberId,
+                                             GenderType genderType,
                                              String accessToken,
                                              String refreshToken) {
         return MemberSignInResponseDto.builder()
                 .memberId(memberId)
+                .gender(genderType.getDesc())
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
